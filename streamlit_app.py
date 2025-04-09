306538f32d241bf87d3873b911ec6ef1e425f639
srilathameda <72034699+srilathameda@users.noreply.github.com>
2025-04-09 15:11:43 -0500

    Update streamlit_app.py
    response 200 text to successful changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,4 +46,4 @@ if ingredients_List:
 
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
