c88f51ce8ae95d6a596007562929c4d5694d78d9
srilathameda <72034699+srilathameda@users.noreply.github.com>
2025-04-09 15:31:03 -0500

    Update streamlit_app.py
    fruits_chosen

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,8 +31,8 @@ if ingredients_List:
     
     for fruits_chosen in ingredients_List:
         ingredients_string += fruits_chosen + ' '
-        st.subheader(fruit_chosen + 'Nutrition Information')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        st.subheader(fruits_chosen + 'Nutrition Information')
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruits_chosen)
         #st.text(smoothiefroot_response.json())
         sf_df  = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
         
